08a2bcd97523ce9769605e9c9aab1c0ed856c0bd
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-14 22:06:08 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -9,4 +9,8 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @total += price * quantity
   end
+
+  def apply_discount(discount)
+    @total *= (1 - (discount / 100)) 
+  end
 end
