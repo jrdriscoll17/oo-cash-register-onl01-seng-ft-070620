97228187708db0b3aaac7af386f498d1a1d1b512
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-14 22:25:15 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   def add_item(title, price, quantity = 1)
     @title = title
     @total += price * quantity
-    @@item << self
+    @@items << self
   end
 
   def apply_discount
