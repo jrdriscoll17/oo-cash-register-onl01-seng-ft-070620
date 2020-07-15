2f9bea7aa37f4c3d2ea025371be321c565f334f9
Jake Driscoll <jrdriscoll17@gmail.com>
2020-07-15 00:21:47 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -31,7 +31,7 @@ class CashRegister
   def void_last_transaction
     self.total -= @last_transaction_price
     @item.pop()
-    if @item.length == 0
+    if @items.length == 0
       self.total = 0.0
     end
   end
